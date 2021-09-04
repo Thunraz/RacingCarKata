0240604569f1ad6d830a62ca0b0f74e3d459afb5
Simon Weis <laguna_1989@gmx.net>
2021-09-04 16:39:00 +0200

    Refactor test code

--- TirePressureMonitoring/tests/Alarm_Test.cpp
+++ TirePressureMonitoring/tests/Alarm_Test.cpp
@@ -1,6 +1,7 @@
 #include "Alarm.h"
 #include "sensor_interface.hpp"
 #include <gmock/gmock.h>
+#include <utility>
 
 using namespace ::testing;
 
@@ -8,64 +9,91 @@ namespace {
 
 class SensorMock : public SensorInterface {
 public:
-    MOCK_METHOD(double, popNextPressurePsiValue, (), (const, override));
+  MOCK_METHOD(double, popNextPressurePsiValue, (), (const, override));
 };
 
-TEST(Alarm, InitialAlarmValueIsOff)
-{
-    SensorMock const pressure_sensor;
-    Alarm const alarm { pressure_sensor };
-    ASSERT_EQ(false, alarm.isAlarmOn());
+class AlarmTest : public Test {
+protected:
+  SensorMock const pressure_sensor;
+  Alarm alarm{pressure_sensor};
+};
+
+TEST_F(AlarmTest, InitialAlarmValueIsOff) {
+  ASSERT_EQ(false, alarm.isAlarmOn());
 }
 
-TEST(Alarm, AlarmIsOnWhenPressureIsBelowDefaultLowThreshold)
-{
-    SensorMock const pressure_sensor;
-    Alarm alarm { pressure_sensor };
+class AlarmTestDefaultLowThresholdTestFixture
+    : public AlarmTest,
+      public WithParamInterface<double> {};
 
-    EXPECT_CALL(pressure_sensor, popNextPressurePsiValue())
-        .WillOnce(Return(3));
+TEST_P(AlarmTestDefaultLowThresholdTestFixture,
+       AlarmIsOnWhenPressureIsBelowDefaultLowThreshold) {
+  EXPECT_CALL(pressure_sensor, popNextPressurePsiValue())
+      .WillOnce(Return(GetParam()));
 
-    alarm.check();
-    ASSERT_EQ(true, alarm.isAlarmOn());
+  alarm.check();
+  ASSERT_EQ(true, alarm.isAlarmOn());
 }
 
-TEST(Alarm, AlarmIsOnWhenPressureIsBelowSetLowThreshold)
-{
-    SensorMock const pressure_sensor;
-    Alarm alarm { pressure_sensor };
+INSTANTIATE_TEST_SUITE_P(AlarmTestLowThresholdTest,
+                         AlarmTestDefaultLowThresholdTestFixture,
+                         Values(3.0, 0.0, 16.999));
 
-    alarm.setLowPressureThreshold(10.0);
-    EXPECT_CALL(pressure_sensor, popNextPressurePsiValue())
-        .WillOnce(Return(9.0));
+class AlarmTestSetLowThresholdTestFixture
+    : public AlarmTest,
+      public WithParamInterface<std::pair<double, double>> {};
 
-    alarm.check();
-    ASSERT_EQ(true, alarm.isAlarmOn());
+TEST_P(AlarmTestSetLowThresholdTestFixture,
+       AlarmIsOnWhenPressureIsBelowSetLowThreshold) {
+  alarm.setLowPressureThreshold(GetParam().first);
+  EXPECT_CALL(pressure_sensor, popNextPressurePsiValue())
+      .WillOnce(Return(GetParam().second));
+
+  alarm.check();
+  ASSERT_EQ(true, alarm.isAlarmOn());
 }
 
-TEST(Alarm, AlarmIsOnWhenPressureIsAboveDefaultHighThreshold)
-{
-    SensorMock const pressure_sensor;
-    Alarm alarm { pressure_sensor };
+INSTANTIATE_TEST_SUITE_P(AlarmTestSetLowThresholdTest,
+                         AlarmTestSetLowThresholdTestFixture,
+                         Values(std::make_pair(11.0, 10.0),
+                                std::make_pair(0.0, -1.0),
+                                std::make_pair(1000.0, 999.9)));
 
-    EXPECT_CALL(pressure_sensor, popNextPressurePsiValue())
-    .WillOnce(Return(33.0));
+class AlarmTestDefaultHighThresholdTestFixture
+    : public AlarmTest,
+      public WithParamInterface<double> {};
 
-    alarm.check();
-    ASSERT_EQ(true, alarm.isAlarmOn());
+TEST_P(AlarmTestDefaultHighThresholdTestFixture,
+       AlarmIsOnWhenPressureIsAboveDefaultHighThreshold) {
+  EXPECT_CALL(pressure_sensor, popNextPressurePsiValue())
+      .WillOnce(Return(GetParam()));
+
+  alarm.check();
+  ASSERT_EQ(true, alarm.isAlarmOn());
 }
 
-TEST(Alarm, AlarmIsOnWhenPressureIsAboveSetHighThreshold)
-{
-    SensorMock const pressure_sensor;
-    Alarm alarm { pressure_sensor };
+INSTANTIATE_TEST_SUITE_P(AlarmTestDefaultHighThresholdTest,
+                         AlarmTestDefaultHighThresholdTestFixture,
+                         Values(33.0, 21.01, 150.0));
 
-    alarm.setHighPressureThreshold(100.0);
-    EXPECT_CALL(pressure_sensor, popNextPressurePsiValue())
-    .WillOnce(Return(101.0));
+class AlarmTestSetHighThresholdTestFixture
+    : public AlarmTest,
+      public WithParamInterface<std::pair<double, double>> {};
 
-    alarm.check();
-    ASSERT_EQ(true, alarm.isAlarmOn());
+TEST_P(AlarmTestSetHighThresholdTestFixture, AlarmIsOnWhenPressureIsAboveSetHighThreshold) {
+  alarm.setHighPressureThreshold(GetParam().first);
+  EXPECT_CALL(pressure_sensor, popNextPressurePsiValue())
+      .WillOnce(Return(GetParam().second));
+
+  alarm.check();
+  ASSERT_EQ(true, alarm.isAlarmOn());
 }
 
-}
+INSTANTIATE_TEST_SUITE_P(AlarmTestSetHighThresholdTest,
+                         AlarmTestSetHighThresholdTestFixture,
+                         Values(std::make_pair(10.0, 11.0),
+                                std::make_pair(-1.0, 0.0),
+                                std::make_pair(999.0, 1000.0)));
+
+
+} // namespace
