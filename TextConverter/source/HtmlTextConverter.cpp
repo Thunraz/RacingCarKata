a3e68a12e58dd47f128d74a0fb9f25f7f78075ff
faraz117 <farazbhatti117@gmail.com>
2021-10-02 16:48:13 +0200

    Filereader

--- TextConverter/source/HtmlTextConverter.cpp
+++ TextConverter/source/HtmlTextConverter.cpp
@@ -3,11 +3,26 @@
 #include "../include/HtmlTextConverter.h"
 #include "../include/StringEscapeUtils.h"
 
+class FileReaderInterface {
+    virtual std::string get_content(std::string const& filename) = 0;
+    virtual std::string get_last_filename() = 0;
+};
+
+class FileReader : public FileReaderInterface{
+public:
+    std::string get_content(std::string const& filename) override{
+        return "";
+    }
+    std::string get_last_filename() {
+        return "";
+    };
+};
+
+
 std::string convert_text_to_html(std::string const& text_to_convert)
 {
     std::string output = text_to_convert;
     output = StringEscapeUtils::escapeHtml(output);
     output = std::regex_replace(output, std::regex("[\n]"), "<br />\n");
-
     return output;
 }
--- TextConverter/tests/HtmlTextConverter_Test.cpp
+++ TextConverter/tests/HtmlTextConverter_Test.cpp
@@ -4,16 +4,14 @@
 namespace {
 
 // TEST(HtmlTextConverterTest, ReturnsCorrectFilename)
-//{
-//     HtmlTextConverter const converter { "foo" };
+// {
 //     ASSERT_EQ("foo", converter.getFilename());
 // }
-//
+
 // TEST(HtmlTextConverterTest, ConvertToHtmlFailsForInvalidFileName)
-//{
-//
+// {
+
 //     auto fn = []() {
-//         HtmlTextConverter converter { "?ThisWontWork?" };
 //         try {
 //             converter.convertToHtml();
 //         } catch (std::invalid_argument& e) {
