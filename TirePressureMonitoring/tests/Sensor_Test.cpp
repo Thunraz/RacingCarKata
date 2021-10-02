e66cf9b72ba7be23c3017231fd3c84c21e1f9f7e
Paul Kerchen <paul@whirlingchair.com>
2021-10-02 09:25:42 -0400

    Removed RNG from tests

--- TirePressureMonitoring/tests/Sensor_Test.cpp
+++ TirePressureMonitoring/tests/Sensor_Test.cpp
@@ -1,29 +1,21 @@
 #include "sensor_interface.hpp"
 #include <gmock/gmock.h>
 #include "Sensor.h"
-#include <utility>
-#include <thread>
-#include <chrono>
 
 using namespace ::testing;
 
-TEST(SensorTest,DefaultSeedRandomness){
-    Sensor A;
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000U));
-    Sensor B;
-    std::vector<double> values_A;
-    std::vector<double> values_B;
-    values_A.push_back(A.popNextPressurePsiValue());
-    values_B.push_back(B.popNextPressurePsiValue());
-    EXPECT_NE(values_A,values_B);
+TEST(SensorTest,DifferentRandomSeedsYieldDifferentPressureValues){
+    Sensor A{ 10};
+    Sensor B{42};
+    double const value_A = A.popNextPressurePsiValue();
+    double const value_B = B.popNextPressurePsiValue();
+    EXPECT_NE(value_A,value_B);
 }
 
-TEST(SensorTest,SeedRandomness){
+TEST(SensorTest,SameRandomSeedYieldsSamePressureValues){
     Sensor A{10};
     Sensor B{10};
-    std::vector<double> values_A;
-    std::vector<double> values_B;
-    values_A.push_back(A.popNextPressurePsiValue());
-    values_B.push_back(B.popNextPressurePsiValue());
-    EXPECT_EQ(values_A,values_B);
+    double const value_A = A.popNextPressurePsiValue();
+    double const value_B = B.popNextPressurePsiValue();
+    EXPECT_EQ(value_A,value_B);
 }
