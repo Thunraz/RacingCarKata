8984eb7d64f2b0524a715343baa9536ef970acc2
faraz117 <farazbhatti117@gmail.com>
2021-10-23 16:30:06 +0200

    Reading file using file system

--- TextConverter/source/FileReader.cpp
+++ TextConverter/source/FileReader.cpp
@@ -1,6 +1,10 @@
 #include "FileReader.hpp"
+#include <filesystem>
+#include <fstream>
 
 std::string FileReader::get_content()
 {
+    std::filesystem::path file_path {m_lastFilename};
+    std::ifstream file {file_path};
     return "";
 }
