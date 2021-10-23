f20eb3ae308726db68fe77eec26d8a886b8b9441
Simon Weis <laguna_1989@gmx.net>
2021-10-23 16:50:05 +0200

    Tinker around with filesystem path

--- TextConverter/include/FileReader.hpp
+++ TextConverter/include/FileReader.hpp
@@ -1,21 +1,20 @@
 #ifndef RACING_CAR_GMOCK_FILEREADER_HPP
 #define RACING_CAR_GMOCK_FILEREADER_HPP
+#include <filesystem>
+
 #include "FileReaderInterface.hpp"
 
 class FileReader : public FileReaderInterface {
 private:
-    std::string m_lastFilename { "" };
+    std::filesystem::path m_lastFilename;
 
 public:
-    FileReader(std::string const& filename)
+    FileReader(std::filesystem::path const& filename)
         : m_lastFilename { filename }
     {
     }
     std::string get_content() override;
-    std::string get_last_filename() const { return m_lastFilename; };
+    std::string get_last_filename() const { return m_lastFilename.string(); };
 };
-
-#include "FileReaderInterface.hpp"
-#include "HtmlTextConverter.h"
 
 #endif // RACING_CAR_GMOCK_FILEREADER_HPP
--- TextConverter/source/FileReader.cpp
+++ TextConverter/source/FileReader.cpp
@@ -1,10 +1,14 @@
 #include "FileReader.hpp"
 #include <filesystem>
 #include <fstream>
+#include <sstream>
 
 std::string FileReader::get_content()
 {
-    std::filesystem::path file_path {m_lastFilename};
-    std::ifstream file {file_path};
-    return "";
+    std::filesystem::path file_path { m_lastFilename };
+    std::ifstream file { file_path };
+    std::stringstream file_buffer;
+
+    file_buffer << file.rdbuf();
+    return file_buffer.str();
 }
