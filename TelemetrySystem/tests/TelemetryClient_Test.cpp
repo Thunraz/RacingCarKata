a0da818411593fc15e09d292d2eed67ccab26961
Simon Weis <laguna_1989@gmx.net>
2021-11-13 16:17:52 +0100

    Add more test for TelemetryClient

--- TelemetrySystem/tests/TelemetryClient_Test.cpp
+++ TelemetrySystem/tests/TelemetryClient_Test.cpp
@@ -1,11 +1,57 @@
-#include <gmock/gmock.h>
 #include "TelemetryClient.h"
+#include <gmock/gmock.h>
 
 namespace {
-    TEST(TelemetryClient, InitialStatusIsOffline) {
-        TelemetryClient tc;
+TEST(TelemetryClient, InitialStatusIsOffline)
+{
+    TelemetryClient tc;
 
-        ASSERT_FALSE(tc.getOnlineStatus());
-    }
+    ASSERT_FALSE(tc.getOnlineStatus());
 }
 
+TEST(TelemetryClient, ConnectWithEmptyConnectStringRaisesException)
+{
+    TelemetryClient tc;
+
+    EXPECT_THROW(tc.connect(""), std::invalid_argument);
+}
+
+// TODO Will fail randomly
+TEST(TelemetryClient, OnlineStatusAfterConnect)
+{
+    TelemetryClient tc;
+    tc.connect("abcd");
+
+    ASSERT_TRUE(tc.getOnlineStatus());
+}
+
+TEST(TelemetryClient, OnlineStatusAfterDisconnect)
+{
+    TelemetryClient tc;
+    tc.connect("abcd");
+    tc.disconnect();
+
+    ASSERT_FALSE(tc.getOnlineStatus());
+}
+
+TEST(TelemetryClient, SendEmptyMessageWillRaiseException)
+{
+    TelemetryClient tc;
+    EXPECT_THROW(tc.send(""), std::invalid_argument);
+}
+
+TEST(TelemetryClient, SendMessageWithContentWillNotRaiseException)
+{
+    TelemetryClient tc;
+    EXPECT_NO_THROW(tc.send("abcd"));
+}
+
+TEST(TelemetryClient, DISABLED_ReceiveWithoutSend)
+{
+    TelemetryClient tc;
+    auto const received_message = tc.receive();
+
+    ASSERT_EQ(received_message, "");
+}
+
+} // namespace
