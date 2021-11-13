f1d49b72adb81324223e6a1b562d1e704ad66fc8
Simon Weis <laguna_1989@gmx.net>
2021-11-13 15:18:32 +0100

    Initial ticket tests

--- TurnTicketDispenser/tests/TicketDispenser_Test.cpp
+++ TurnTicketDispenser/tests/TicketDispenser_Test.cpp
@@ -1,7 +1,34 @@
-#include <gmock/gmock.h>
 #include "TicketDispenser.h"
+#include <gmock/gmock.h>
 
 namespace {
-  TEST(TicketDispenser, TBD) {
-  }
+
+class TurnTicketTestFixture : public ::testing::TestWithParam<int> {
+};
+
+TEST_P(TurnTicketTestFixture, TicketReturnsTicketNumber)
+{
+    auto const ticket_number = GetParam();
+    TurnTicket ticket { ticket_number };
+    ASSERT_EQ(ticket.getTurnNumber(), ticket_number);
 }
+
+INSTANTIATE_TEST_SUITE_P(TurnTicketTest, TurnTicketTestFixture, ::testing::Values(5, 42, 125));
+
+TEST(TicketDispenser, InitialTicketHasNumber0)
+{
+    TicketDispenser ticket_dispenser;
+    auto ticket = ticket_dispenser.getTurnTicket();
+
+    ASSERT_EQ(ticket.getTurnNumber(), 0);
+}
+
+TEST(TicketDispenser, SecondTicketHasNumber1)
+{
+    TicketDispenser ticket_dispenser;
+    auto ticket = ticket_dispenser.getTurnTicket();
+
+    ASSERT_EQ(ticket.getTurnNumber(), 1);
+}
+
+} // namespace
