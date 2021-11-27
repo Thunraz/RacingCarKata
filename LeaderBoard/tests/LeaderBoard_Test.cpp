9ad1393b5eaad6ea27ff0da4a380a756dce2da6d
Paul Kerchen <paul@whirlingchair.com>
2021-11-27 11:01:36 -0500

    Implemented leaderboard tests

--- LeaderBoard/tests/LeaderBoard_Test.cpp
+++ LeaderBoard/tests/LeaderBoard_Test.cpp
@@ -2,8 +2,33 @@
 #include "LeaderBoard.h"
 
 namespace {
-  TEST(LeaderBoard, TBD) {
+  TEST(LeaderBoard, EmptyListOfRacesReturnsEmptyResults) {
+      Leaderboard board{ {} };
 
+      ASSERT_TRUE(board.driverResults().empty());
   }
+
+  TEST(LeaderBoard, EmptyListOfRacesReturnsEmptyRankings) {
+        Leaderboard board{ {} };
+
+        ASSERT_TRUE(board.driverRankings().empty());
+    }
+
+    TEST(LeaderBoard, SingleDriverAppearsInResults) {
+        auto driver1 = std::make_shared<Driver>("name", "country");
+        Race race("whocares", { driver1 });
+        Leaderboard board{ {race} };
+
+        ASSERT_EQ(board.driverResults().count("name"), 1);
+    }
+
+    TEST(LeaderBoard, SingleDriverHasCorrectNumberOfPoints) {
+        auto driver1 = std::make_shared<Driver>("name", "country");
+        Race race("whocares", { driver1 });
+        Leaderboard board{ {race} };
+
+        ASSERT_EQ(board.driverResults().at("name"), 25);
+    }
+
 }
 
